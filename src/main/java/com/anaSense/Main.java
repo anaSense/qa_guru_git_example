0c49af326d18355d14dcfccc3aa04f3ca3478e55
annaegorova <anna-khabarova2107@yandex.ru>
2023-12-28 22:10:33 +0300

    randomize message

--- src/main/java/com/anaSense/Main.java
+++ src/main/java/com/anaSense/Main.java
@@ -1,7 +1,24 @@
 package com.anaSense;
 
+import java.util.Random;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello QA GURU!");
+        System.out.println(getMessage());
+    }
+
+    public static String getMessage() {
+        Random rand = new Random();
+        int i = rand.nextInt(5);
+        System.out.println("i = "+i);
+        String message = "";
+        if (i == 0) {
+            return message = "Hello QA GURU!";
+        } else if (i == 1) {
+            return message = "Hello World!";
+        } else {
+            return message = "Hello IntelliJ IDEA!";
+        }
     }
 }
